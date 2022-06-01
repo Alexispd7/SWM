8257d4a9772a2b9a02a18bed7f8febae0cc9b61f
Alexispd7 <alexispd7@gmail.com>
2022-06-01 14:55:04 +0300

    new12

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,53 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
+import Technologies from "./Technologies";
+import Footer from "./Footer";
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+
+            <header className='header'>
+                <img
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF-gzBtjN0Dwj0tRp9KcKew4l5dtmPsfepHw&usqp=CAU'/>
+            </header>
+            <nav className='nav'>
+                <div><a>
+                    Profile
+                </a></div>
+                <div><a>
+                    Messages
+                </a></div>
+                <div><a>
+                    News
+                </a></div>
+                <div><a>
+                    Music
+                </a></div>
+                <div><a>
+                    Settings
+                </a></div>
+            </nav>
+            <div className='content'>
+                <div><img
+                    src='https://www.grekomania.ru/images/greek-articles/places/big/229_Navagio-Beach-Zakynthos.jpg'/>
+                </div>
+                <div>Ava + DD</div>
+                <div>My Posts
+                    <div>New Post</div>
+                    <div>
+                        <div>Post1</div>
+                        <div>Post2</div>
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    );
 }
+
 
 export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <a href='#s'>Home</a>
+            <a href='#s'>News</a>
+            <a href='#s'>Messages</a>
+            <a href='#s'>Home</a>
+        </div>
+    );
+}
+export  default Header;
